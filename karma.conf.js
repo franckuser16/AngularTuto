1e4346218a550df52d7c69abc7eea3d9bc5fae82
Franck Michaud <franckuser16@gmail.com>
2014-12-21 22:36:28 +0100

    added coverage integration to travis and codeclimate

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,23 @@ module.exports = function(config){
     reporters: ['progress', 'coverage'],
     preprocessors: { '*.js': ['coverage'] },
 
+    coverageReporter: {
+      // specify a common output directory
+      dir: 'coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
+    },
+
     junitReporter : {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
